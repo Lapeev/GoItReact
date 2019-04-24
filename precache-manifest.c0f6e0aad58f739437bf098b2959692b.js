42c05d6744c2322c2567d2f5d6977d68717ca394
lapeev16@gmail.com <lapeev16@gmail.com>
2019-04-24 17:17:57 +0300

    Updates

---
+++ precache-manifest.c0f6e0aad58f739437bf098b2959692b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9004991824886f2f9c96",
+    "url": "/GoItReact/static/js/runtime~main.f26a4b3e.js"
+  },
+  {
+    "revision": "07b4816a049bbc78c79c",
+    "url": "/GoItReact/static/js/main.1d5780d4.chunk.js"
+  },
+  {
+    "revision": "c31b5ecc3c8d85e17e7d",
+    "url": "/GoItReact/static/js/2.44e0a5b2.chunk.js"
+  },
+  {
+    "revision": "07b4816a049bbc78c79c",
+    "url": "/GoItReact/static/css/main.bc0ad794.chunk.css"
+  },
+  {
+    "revision": "72242925b05a5857d83638bf99811245",
+    "url": "/GoItReact/index.html"
+  }
+];
